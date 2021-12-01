dcb19a504f0029983410e45d537af8310560a07a
Kevin Lien <k4ito@outlook.com>
2021-12-01 08:02:43 +0100

    day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,43 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var cnt = 0;
+        for (i in input.indices) {
+            if (i == 0) continue;
+
+            val currentValue = input[i].toInt();
+            val previousValue = input[i-1].toInt();
+
+            if (currentValue > previousValue) cnt++;
+        }
+        return cnt
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var cnt = 0;
+        for (i in input.indices) {
+            if (i + 3 >= input.size) return cnt;
+
+            val currentValue = input[i].toInt();
+            val nextValue = input[i+1].toInt();
+            val nextNextValue = input[i+2].toInt();
+            val firstValue = currentValue + nextValue + nextNextValue;
+
+            val currentValueSecondHalf = input[i+1].toInt();
+            val nextValueSecondHalf = input[i+2].toInt();
+            val nextNextValueSecondHalf = input[i+3].toInt();
+            val secondValue = currentValueSecondHalf + nextValueSecondHalf + nextNextValueSecondHalf;
+
+            if (secondValue > firstValue) cnt++;
+        }
+        return cnt
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+
+    val testInput2 = readInput("Day01_test")
+    check(part2(testInput2) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
