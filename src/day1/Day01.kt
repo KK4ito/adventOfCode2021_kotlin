96ae3d5b360773f06c50e9f26859c503159afa92
Kevin Lien <k4ito@outlook.com>
2021-12-01 18:53:41 +0100

    day1 solution prettier

--- src/day1/Day01.kt
+++ src/day1/Day01.kt
@@ -4,36 +4,41 @@ import readInput
 
 fun main() {
     fun part1(input: List<String>): Int {
-        var cnt = 0;
-        for (i in input.indices) {
-            if (i == 0) continue;
-
-            val currentValue = input[i].toInt();
-            val previousValue = input[i-1].toInt();
-
-            if (currentValue > previousValue) cnt++;
-        }
-        return cnt
+//        var cnt = 0;
+//        for (i in input.indices) {
+//            if (i == 0) continue;
+//
+//            val currentValue = input[i].toInt();
+//            val previousValue = input[i-1].toInt();
+//
+//            if (currentValue > previousValue) cnt++;
+//        }
+//        return cnt
+        val inputAsInt = input.map { it.toInt() }
+        return inputAsInt.windowed(2).count { (x, y) -> y > x }
     }
 
     fun part2(input: List<String>): Int {
-        var cnt = 0;
-        for (i in input.indices) {
-            if (i + 3 >= input.size) return cnt;
+//        var cnt = 0;
+//        for (i in input.indices) {
+//            if (i + 3 >= input.size) return cnt;
+//
+//            val currentValue = input[i].toInt();
+//            val nextValue = input[i+1].toInt();
+//            val nextNextValue = input[i+2].toInt();
+//            val firstValue = currentValue + nextValue + nextNextValue;
+//
+//            val currentValueSecondHalf = input[i+1].toInt();
+//            val nextValueSecondHalf = input[i+2].toInt();
+//            val nextNextValueSecondHalf = input[i+3].toInt();
+//            val secondValue = currentValueSecondHalf + nextValueSecondHalf + nextNextValueSecondHalf;
+//
+//            if (secondValue > firstValue) cnt++;
+//        }
+//        return cnt
+        val inputAsInt = input.map { it.toInt() }
+        return inputAsInt.windowed(3).map { it.sum() }.windowed(2).count { (x, y) -> y > x }
 
-            val currentValue = input[i].toInt();
-            val nextValue = input[i+1].toInt();
-            val nextNextValue = input[i+2].toInt();
-            val firstValue = currentValue + nextValue + nextNextValue;
-
-            val currentValueSecondHalf = input[i+1].toInt();
-            val nextValueSecondHalf = input[i+2].toInt();
-            val nextNextValueSecondHalf = input[i+3].toInt();
-            val secondValue = currentValueSecondHalf + nextValueSecondHalf + nextNextValueSecondHalf;
-
-            if (secondValue > firstValue) cnt++;
-        }
-        return cnt
     }
 
     // test if implementation meets criteria from the description, like:
@@ -46,4 +51,6 @@ fun main() {
     val input = readInput("Day01", "day1")
     println(part1(input))
     println(part2(input))
+
+
 }
